0c20cea659437896296a87d7a18ff0eda7eea473
Raidy <raidi16150905@gmail.com>
2026-05-20 07:44:15 -0700

    Add project files.

---
+++ WheatherAppLayoutView/Controllers/WeatherController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WheatherAppLayoutView.Controllers
+{
+    public class WeatherController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WheatherAppLayoutView/Models/CityWheater.cs
@@ -0,0 +1,10 @@
+﻿namespace WheatherAppLayoutView.Models
+{
+    public class CityWheater
+    {
+        public string CityUniqueCode { get; set; }
+        public string CityName { get; set; }
+        public DateTime? DateAndTime { get; set; }
+        int Temperature { get; set; }
+    }
+}
